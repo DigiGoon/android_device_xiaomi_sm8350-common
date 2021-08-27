84966c56c8873f4000c3414519d6e165ce57be00
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm8350-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- common.mk
+++ common.mk
@@ -400,9 +400,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
-PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QTI
