0560c8c90ec71632c6fb02609debeea09139d11f
ralph950412 <ralph950412@gmail.com>
2023-02-01 15:31:03 +0800

    sm8350-common: Build thermal HAL
    * Perf blobs look for it
    
    Change-Id: I589d7622657536fdc764da94184dcbb21820de39
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -469,6 +469,8 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
