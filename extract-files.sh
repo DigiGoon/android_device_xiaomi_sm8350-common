fe2f6ea829fe1388986ec044c2f2894f9f81dd57
Jake Weinstein <jake@aospa.co>
2023-06-07 16:14:10 +0300

    sm8350-common: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    And also affine them.

--- extract-files.sh
+++ extract-files.sh
@@ -78,6 +78,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        vendor/etc/msm_irqbalance.conf)
+            sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
+            ;;
         vendor/etc/media_lahaina/video_system_specs.json \
         |vendor/etc/media_shima_v1/video_system_specs.json \
         |vendor/etc/media_yupik_v1/video_system_specs.json)
