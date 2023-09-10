cc1f7c9709200f40d974e60526f91748c495a73b
madmax7896 <madmax78968@gmail.com>
2023-09-10 10:16:28 -0300

    alioth: Import Dolby audio blobs
     * This reverts https://github.com/AOSPA/android_device_xiaomi_alioth/commit/e421686adb9fb4abf8e2a198f68529f9694b9cf0
       and partially reverts https://github.com/AOSPA/android_device_xiaomi_alioth/commit/c8c916d09a42a678bd8ef0217e126601966fb4e8
    
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- common.mk
+++ common.mk
@@ -310,7 +310,13 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
-    libstagefrighthw
+    libstagefrighthw \
+    libavservices_minijail_vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
