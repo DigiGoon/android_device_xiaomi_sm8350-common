69997405f60f8e620a9908cdb7fc4d768a7591ba
Dyneteve <dyneteve@hentaios.com>
2023-10-02 19:03:11 +0300

    sm8350-common: Build TextClassifier packages
    - Helps fix the delay we have in copying text and that being available in clipboard
    
    Co-authored-by: marshmello61 <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -459,6 +459,13 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+	libtextclassifier_annotator_en_model \
+	libtextclassifier_annotator_universal_model \
+	libtextclassifier_actions_suggestions_universal_model \
+	libtextclassifier_lang_id_model
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
