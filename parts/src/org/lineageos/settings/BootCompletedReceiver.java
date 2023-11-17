f76e9cc924ace8dac6546a59448c26bba2aa21ef
Emanuel <emanuelghub@gmail.com>
2023-11-17 23:56:08 +0530

    [SQUASH] sm8350-common: parts: Import DC Dimming and HBM
    this squash imports DC Dimming and HBM to xiaomi parts and rootdir
    it is adjusted to for sm8350
    (*＞ω＜*)♡
    
    inspired by
    
    HBM commit:
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/87eab66cf179679143cb904b118358ed74246299
    
    DC dimming commits:
    
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/6db27e28ef6e01ec092f43b20d072f61c61c1a33
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/49be770010693b49db1628242b6902ee942c6f42
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/1e4393d160d5eef8a51e135fa83e621c50a47b13
    
    blazerpaul15's edit:
    - Make hbm state writable by system
    - Fix HBM path
    
    Co-authored-by: Arian <39061882+ArianK16a@users.noreply.github.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: kamikaonashi <thorschi93@gmx.at>
    Co-authored-by: blazerpaul15 <blazerpaul15@gmail.com>
    Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -10,18 +10,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
     private static final boolean DEBUG = true;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
         if (!intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
             return;
         }
@@ -32,5 +40,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Thermal
         ThermalUtils.startService(context);
+
+        // DC Dimming
+        FileUtils.enableService(context);
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }
