906f15994ee08c47b7ac2660aadf386989bf3164
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-07 15:40:03 +0300

    sm8350-common: Introduce Dolby Atmos app
    Converted existing dirac implementation based on observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,6 +183,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/dolby
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Soong
--- common.mk
+++ common.mk
@@ -177,6 +177,14 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
