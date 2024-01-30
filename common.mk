e9b524ca243bf72fe8b889df814a3c27e42e756a
AdarshGreal <adarshgrewal@gmail.com>
2024-01-30 14:03:28 +0530

    sm8350-common: Setup kernel modules for PA
     * Only keep common modules in common tree
     * Drop touch modules from recovery as AOSPA recovery has not touch support
    
    Change-Id: Id7fbdb133f2a585c48b78e09a2fabb30b4a48c4e
    Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

--- common.mk
+++ common.mk
@@ -266,6 +266,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Kernel
+KERNEL_MODULES_INSTALL := dlkm
+KERNEL_MODULES_OUT := $(OUT_DIR)/target/product/$(AOSPA_BUILD)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
