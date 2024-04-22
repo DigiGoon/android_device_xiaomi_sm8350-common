88ecebe9fce2a80223a0565df14c0a62dcc40cb1
AdarshGrewal <adarshgrewal@gmail.com>
2024-04-23 00:32:54 +0530

    sm8350-common: Setup kernel
     * Drop BOARD_KERNEL_IMAGE_NAME already defined
     * Drop TARGET_KERNEL_ADDITIONAL_FLAGS aospa does not support this idk what to do about it for now
     * Drop TARGET_KERNEL_SOURCE lets just use the msm-5.4 dir
     * Merge lahaina-qgki_defconfig, debugfs.config & xiaomi_QGKI.config aospa's config fragment only allows to use one fragment so just merge the common stuff
    
    Change-Id: I08971f0eb26d6923e69aa1e5b40426d40d7e5d63

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,17 +75,13 @@ DEVICE_MANIFEST_FILE := \
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
-TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
-TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config
+KERNEL_DEFCONFIG := vendor/xiaomi_lahaina-qgki_defconfig
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
