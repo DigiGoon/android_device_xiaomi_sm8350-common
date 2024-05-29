8067416c101515615c42ddb742cfafe44bc99a47
basamaryan <basam.aryan@gmail.com>
2024-05-30 00:31:13 +0530

    sm8350-common: Build XiaomiDolby
    Change-Id: I31ff2c27a479423951c8a645bf1cd65dc6ad6f81
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -143,7 +143,8 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    XiaomiDolby
 
 # Display
 PRODUCT_PACKAGES += \
